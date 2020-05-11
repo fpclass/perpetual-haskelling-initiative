c4e7a9ce6e2ee128df8b8edb66536f6ad6357658
Michael B. Gale <m.gale@warwick.ac.uk>
2020-05-11 16:15:52 +0100

    Add initial project structure (#2)

---
+++ CONTRIBUTING.md
@@ -0,0 +1,11 @@
+# Contributors Guide
+
+Clone the repository with `git clone https://github.com/fpclass/perpetual-haskelling-initiative`. 
+
+## Branching
+
+We follow [Gitflow](https://www.atlassian.com/git/tutorials/comparing-workflows/gitflow-workflow) for this project. New features should be developed on branches prefixed with `feature/`. For example, after cloning the repository, you may wish to create a new branch for your feature by running `git checkout -b feature/my-feature-name` where `my-feature-name` is a short description of the feature.
+
+## Commits 
+
+Commit messages should be written according to [this guide](https://github.com/RomuloOliveira/commit-messages-guide).
---
+++ app/Main.hs
@@ -0,0 +1,18 @@
+-------------------------------------------------------------------------------
+-- Perpetual Haskelling Initiative                                           --
+-------------------------------------------------------------------------------
+-- This source code is licensed under the MIT licence found in the           --
+-- LICENCE file in the root directory of this source tree.                   --
+-------------------------------------------------------------------------------
+
+-- | This module contains the main entry point for the Perpetual Haskelling
+-- Initiative executable.
+module Main ( main ) where 
+
+-------------------------------------------------------------------------------
+
+-- | 'main' is the main entry point for this application.
+main :: IO ()
+main = putStrLn "Hello World!"
+
+-------------------------------------------------------------------------------
