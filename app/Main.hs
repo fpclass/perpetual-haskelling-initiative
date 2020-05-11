f3eac7cafad36de0a80046535e3f243777062c11
Oscar Harris <ozj.harris@gmail.com>
2020-05-11 18:40:09 +0100

    Add a menu to app/Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -11,8 +11,58 @@ module Main ( main ) where
 
 import System.IO
 
--------------------------------------------------------------------------------
+-- Temporary Definitions - When Card/Deck are defined in the library this will be
+-- removed
+type Card = String
+type Deck = [Card]
 
+loadDeck :: IO (Maybe Deck)
+loadDeck = undefined
+
+saveDeck :: Maybe Deck -> IO ()
+saveDeck = undefined
+
+createNewDeck :: IO Deck
+createNewDeck = undefined
+
+editCurrentDeck :: Maybe Deck -> IO Deck
+editCurrentDeck = undefined
+
+play :: Maybe Deck -> IO ()
+play = undefined
+
+menu :: Maybe Deck -> IO ()
+menu deck = do
+    -- Determines whether to show options that require a deck to be present
+    let deckOptions = if deck == Nothing then 
+                          ""
+                      else
+                          "\tS) Save Deck\n\tE) Edit Current Deck\n\tP) Play"
+    putStrLn "Welcome to the Perpetual Haskelling Initiative!\n"
+    putStrLn "Menu:"
+    putStrLn "\tL) Load Deck"
+    putStrLn "\tC) Create New Deck"
+    putStrLn $ deckOptions ++ "\tQ) Quit"
+    putStr ">>> "
+
+    choice <- getLine
+
+    case choice of
+        "L" -> loadDeck >>= menu
+        "S" -> do
+            saveDeck deck
+            menu deck
+        "C" -> createNewDeck >>= menu . Just
+        "E" -> editCurrentDeck deck >>= menu . Just
+        "P" -> do
+            play deck
+            menu deck
+        "Q" -> pure ()
+        _ -> do
+            putStrLn "Invalid Choice"
+            menu deck
+
+-------------------------------------------------------------------------------
 -- | 'main' is the main entry point for this application.
 main :: IO ()
 main = do
@@ -20,6 +70,5 @@ main = do
     hSetBuffering stdout NoBuffering
     hSetBuffering stdin  NoBuffering
 
-    putStrLn "Hello World!"
-
+    menu Nothing
 -------------------------------------------------------------------------------
