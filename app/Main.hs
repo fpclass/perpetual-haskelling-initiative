e814bf6cf7b81ffac0dc63edea7d17d240fe6892
Oscar Harris <ozj.harris@gmail.com>
2020-05-12 12:25:13 +0100

    Define `makeCard` function in app/Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -12,15 +12,35 @@ module Main ( main ) where
 import System.IO
 import Data.Char (toUpper)
 import Data.Aeson
+import qualified Data.Text as T
+import qualified Data.Text.IO as T (putStrLn, getLine)
+import qualified Data.List.NonEmpty as NE
+import Data.List (nub)
+import Text.Read (readMaybe)
 import System.FilePath
-import Control.Monad
+import Control.Monad (forM)
 
 -- Temporary Definitions - When Card/Deck are defined in the library this will be
 -- removed
 type Card = String
 type Deck = [Card]
+data Paradigm = NotYetImplemented1
+    deriving (Show, Read)
+data Instr = NotYetImplemented2
+    deriving (Show)
+type Program  = [Instr]
 cardsInDeck :: Int
 cardsInDeck = 10
+cardName :: b -> T.Text
+cardName = const ""
+cardDescription :: b -> T.Text
+cardDescription = const ""
+cardParadigms :: b -> NE.NonEmpty Paradigm
+cardParadigms = const $ NE.fromList []
+cardProgram :: b -> Program
+cardProgram = const []
+parseProg :: [String] -> Maybe Program
+parseProg = undefined
 
 -- | `loadDeck` is a computation which tries to load a deck from disk.
 loadDeck :: IO (Maybe Deck)
@@ -45,7 +65,64 @@ saveDeck d = do
 -- | `makeCard` prompts the user through creating cards. It optionally takes a
 --   card which is will use as default values
 makeCard :: Maybe Card -> IO Card
-makeCard = undefined
+makeCard c = do
+    name <- prompt "\nEnter Card Name" $ cardName <$> c
+    desc <- prompt "\nEnter Card Description" $ cardDescription <$> c
+    -- (mapM readMaybe) passes a list of strings to a list of Paradigms if possible. Since paradigms is NonEmpty not [] it must
+    -- be converted to a list and then from a list again after
+    paras <- fmap NE.fromList $ promptMult "\nEnter Card Paradigms:" 1 (mapM readMaybe) $ NE.toList . cardParadigms <$> c
+    prog <- promptMult "\nEnter Card Program:" 0 parseProg $ cardProgram <$> c
+
+    -- Will be replaced with instance of card when possible
+    pure ""
+
+    where
+        -- | `prompt` takes a prompt and optionally a default value and gets valid (non-empty) text
+        --   input
+        prompt :: String -> Maybe T.Text -> IO T.Text
+        prompt s d = do
+            putStr s 
+            -- If default value is given then show it in brackets
+            T.putStrLn $ maybe "" (\x -> T.append " (" $ T.append x "):") d
+            putStr "> "
+            input <- T.getLine
+
+            if input == "" then
+                -- If blank and default exists use default, otherwise run again
+                maybe (putStrLn "Input Cannot Be Blank" >> prompt s d) pure d
+            else
+                pure input
+
+        -- | `promptMult` takes a prompt, a minimum length, a parsing function and optionally a default
+        --   value and gets a valid list of the given type
+        promptMult :: (Show a) => String -> Int -> ([String] -> Maybe [a]) -> Maybe [a] -> IO [a]
+        promptMult s minLines parse d = do
+            putStr s
+            putStrLn $ maybe "" (\x -> mappend " (" $ mappend x "):") $ show <$> d
+            putStrLn "Press enter on a blank line to finish (press enter on the first line to use the default value)"
+            -- Get multiple lines and remove duplicates
+            input <- nub <$> getLines
+            let parsedInput = parse input
+
+            case parsedInput of
+                Nothing -> putStrLn "Invalid Input" >> promptMult s minLines parse d
+                Just i
+                    | null i -> maybe (putStrLn "Input Cannot Be Blank" >> promptMult s minLines parse d) pure d
+                    | length i < minLines -> do
+                            putStrLn $ "You must enter at least " ++ show minLines ++ " lines." 
+                            promptMult s minLines parse d
+                    | otherwise -> pure i 
+        
+        -- | `getLines` gets repeated user input until they enter a blank line
+        getLines :: IO [String]
+        getLines = do
+            putStr "> "
+            x <- getLine
+            if x == "" then 
+                return []
+            else do
+                xs <- getLines
+                return (x:xs)
 
 -- | `createNewDeck` is a computation which tries to create a new deck
 createNewDeck :: IO Deck
