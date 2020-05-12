c1e260ceb221e0d6ea34f00a24e774c68aa8e45f
Michael B. Gale <m.gale@warwick.ac.uk>
2020-05-12 14:46:51 +0100

    Basic game types (#9)
    * Add basic game types
    * Add FromJSON/ToJSON instances to basic types (#10)
    
    Co-authored-by: Oscar H <34679874+oscar-h64@users.noreply.github.com>

---
+++ src/Purestone/Card.hs
@@ -0,0 +1,42 @@
+-------------------------------------------------------------------------------
+-- Perpetual Haskelling Initiative                                           --
+-------------------------------------------------------------------------------
+-- This source code is licensed under the MIT licence found in the           --
+-- LICENSE file in the root directory of this source tree.                   --
+-------------------------------------------------------------------------------
+module Purestone.Card ( Card(..) ) where 
+
+-------------------------------------------------------------------------------
+
+import Data.List.NonEmpty
+import Data.Text
+import Data.Aeson
+import GHC.Generics
+
+import Purestone.Paradigm
+import Purestone.Language.Program
+import Purestone.JSON
+
+-------------------------------------------------------------------------------
+
+-- | Represents cards in the game.
+data Card = Card {
+    -- | The name of the card.
+    cardName :: Text,
+    -- | The card's flavour text.
+    cardDescription :: Text,
+    -- | The list of paradigms for this card.
+    cardParadigms :: NonEmpty Paradigm,
+    -- | The card's program.
+    cardProgram :: Program
+} deriving (Eq, Show, Generic)
+
+
+-- Define FromJSON and ToJSON instances for Card. genericParseJSON jsonOpts automatically 
+-- derives instance using the field name with the card prefix removed as the key
+instance FromJSON Card where
+    parseJSON = genericParseJSON jsonOpts
+instance ToJSON Card where
+    toJSON = genericToJSON jsonOpts
+
+-------------------------------------------------------------------------------
