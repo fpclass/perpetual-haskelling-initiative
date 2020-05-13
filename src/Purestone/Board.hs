2d56846dbcffaee704a9aaa63192d81016c2d75c
Oscar Harris <ozj.harris@gmail.com>
2020-05-13 20:05:18 +0100

    Add FromJSON/ToJSON instances to Board and Player

--- src/Purestone/Board.hs
+++ src/Purestone/Board.hs
@@ -10,18 +10,23 @@ module Purestone.Board ( Board ) where
 -------------------------------------------------------------------------------
 
 import Purestone.Player
+import Purestone.JSON
+import Data.Aeson
 import GHC.Generics
 
 -------------------------------------------------------------------------------
 
 -- | Represents the board during a match
 data Board
-  = Board {
-    -- | The first player in a match
-    boardPlayer1 :: Player,
-    -- | The second player in a match
-    boardPlayer2 :: Player
-  } deriving (Eq, Show, Generic)
+    = Board {
+        -- | The first player in a match
+        boardPlayer1 :: Player,
+        -- | The second player in a match
+        boardPlayer2 :: Player
+    } deriving (Eq, Show, Generic)
 
-
+instance FromJSON Board where
+    parseJSON = genericParseJSON jsonOpts
+instance ToJSON Board where
+    toJSON = genericToJSON jsonOpts
 -------------------------------------------------------------------------------
