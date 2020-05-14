5fe5459b0f18887253298330fe679a3d39dc2b93
ZiadAmr <ziad2ziad2@gmail.com>
2020-05-14 23:59:40 +0100

    Defined behaviour for "HEAL n" and "ATTACK n"

--- src/Purestone/Interpreter.hs
+++ src/Purestone/Interpreter.hs
@@ -5,13 +5,56 @@
 -- LICENSE file in the root directory of this source tree.                   --
 -------------------------------------------------------------------------------
 
-module Purestone.Interpreter
+module Purestone.Interpreter where
 
 -------------------------------------------------------------------------------
 
-import Purestone.Card
+import Purestone.Language.Program
+
+import Data.Text (Text)
+import Data.Void
+import Data.Either
+
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
 
 -------------------------------------------------------------------------------
+type Error = String
 
-interpreter :: [String] -> Maybe Program
-interpreter = undefined
+type Parser = Parsec Void String
+
+
+interpreter :: [String] -> Maybe Action
+interpreter [] = Just []
+interpreter (instr : instrs)= do 
+                    x  <- translate instr
+                    xs <- interpreter instrs
+                    Just (x:xs)
+
+
+
+-- Translates a line of code into an instruction
+translate :: String -> Maybe Instr
+-- drop 7 w
+translate line  | (isRight p) && (isRight s) && (w == "ATTACK")    = Just (Attack (fromInteger n) )
+                | (isRight p) && (isRight s) && (w == "HEAL")      = Just (Heal (fromInteger n) )
+                | otherwise = Nothing 
+                where   p = parse getWord "" line
+                        Right w = p 
+                        s = parse (integer <* eof) "" (drop ((length w)+1) line)
+                        Right n = s
+
+sc :: Parser ()
+sc = L.space space1 (L.skipLineComment "//") (L.skipBlockComment "/*" "*/")
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+integer :: Parser Integer
+integer = lexeme L.decimal
+
+getWord :: Parser [Char]
+getWord = (many (satisfy ( /= ' ' ) :: Parser Char ) :: Parser [Char] )
+              
