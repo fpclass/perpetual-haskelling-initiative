0f71275a3346891df688620c4ea3a97b0e9fdcdb
Oscar Harris <ozj.harris@gmail.com>
2020-05-14 12:12:40 +0100

    Change `atomicModifyIORef` to `atomicWriteIORef`

--- src/Purestone/Server/GetState.hs
+++ src/Purestone/Server/GetState.hs
@@ -11,11 +11,11 @@ import Purestone.Server.GameState
 stateResponse :: IORef GameState -- ^ IORef to the GameState
               -> Board           -- ^ Board to return to the user if there is changes
               -> Bool            -- ^ Boolean of whether there is new changes for the player, if False HTTP304 is returned
-              -> (GameState -> (GameState, ())) -- ^ Function used to edit IORef to show player now has latest changed
+              -> GameState       -- ^ New GameState after responding to user
               -> Handler Board
-stateResponse s b c f = 
+stateResponse s b c s' = 
     if c then do
-        liftIO $ atomicModifyIORef s f
+        liftIO $ atomicWriteIORef s s'
         pure b
     else
         throwError err304
@@ -29,7 +29,7 @@ getState s _ p = do
     -- If there is no board then return 404, otherwise determine response
     flip (maybe $ throwError err404) b $ \b' -> 
         case p of
-            1 -> stateResponse s b' u1 $ \(b', _, u2) -> ((b', False, u2), ())
-            2 -> stateResponse s b' u2 $ \(b', u1, _) -> ((b', u1, False), ())
+            1 -> stateResponse s b' u1 (b, False, u2)
+            2 -> stateResponse s b' u2 (b, u1, False)
             _ -> throwError err400
 
