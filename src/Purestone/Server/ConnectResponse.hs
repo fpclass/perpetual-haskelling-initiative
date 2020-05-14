346d826c0d5943ca7117c5956e234b77be3457ca
Oscar Harris <ozj.harris@gmail.com>
2020-05-14 12:23:51 +0100

    Add `ConnectResponse` type

---
+++ src/Purestone/Server/ConnectResponse.hs
@@ -0,0 +1,20 @@
+module Purestone.Server.ConnectResponse ( ConnectResponse(..) ) where
+
+import Data.Aeson
+import GHC.Generics
+
+import Purestone.JSON
+
+-- | `ConnectResponse` is what is sent by the server upon successful joining of a game
+data ConnectResponse = 
+    Connected {
+        -- | `connectedGameId` is the game ID of the game joined
+        connectedGameId :: Int,
+        -- | `connectedPlayer` is the player number (1 or 2)
+        connectedPlayer :: Int
+    } deriving (Generic)
+
+instance FromJSON ConnectResponse where
+    parseJSON = genericParseJSON jsonOpts
+instance ToJSON ConnectResponse where
+    toJSON = genericToJSON jsonOpts
