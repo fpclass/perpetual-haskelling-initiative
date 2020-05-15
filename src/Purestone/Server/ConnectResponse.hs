8cf9c6468216c69095b8e8cd7bd804cc9164a83a
Oscar Harris <ozj.harris@gmail.com>
2020-05-15 14:04:08 +0100

    Add license information

--- src/Purestone/Server/ConnectResponse.hs
+++ src/Purestone/Server/ConnectResponse.hs
@@ -1,3 +1,9 @@
+-------------------------------------------------------------------------------
+-- Perpetual Haskelling Initiative                                           --
+-------------------------------------------------------------------------------
+-- This source code is licensed under the MIT licence found in the           --
+-- LICENSE file in the root directory of this source tree.                   --
+-------------------------------------------------------------------------------
 module Purestone.Server.ConnectResponse ( ConnectResponse(..) ) where
 
 import Data.Aeson
--- src/Purestone/Server/MakeMove.hs
+++ src/Purestone/Server/MakeMove.hs
@@ -1,3 +1,9 @@
+-------------------------------------------------------------------------------
+-- Perpetual Haskelling Initiative                                           --
+-------------------------------------------------------------------------------
+-- This source code is licensed under the MIT licence found in the           --
+-- LICENSE file in the root directory of this source tree.                   --
+-------------------------------------------------------------------------------
 module Purestone.Server.MakeMove (makeMove) where
 
 import Servant
