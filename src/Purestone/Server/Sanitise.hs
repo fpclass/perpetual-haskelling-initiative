4994691a07ea121e5a6257486d1d787b116464fd
Oscar Harris <ozj.harris@gmail.com>
2020-05-15 11:10:54 +0100

    Define Sanitise Function

--- src/Purestone/Server/Sanitise.hs
+++ src/Purestone/Server/Sanitise.hs
@@ -1,6 +1,12 @@
 module Purestone.Server.Sanitise ( sanitiseBoard ) where
 
+import Purestone.Player
 import Purestone.Board
 
+sanitisePlayer :: Player -> Player
+sanitisePlayer p = p { playerHand = [], playerDeck = [] }
+
 sanitiseBoard :: Board -> Int -> Board
-sanitiseBoard = undefined
+sanitiseBoard b p = case p of
+    1 -> b { boardPlayer2 = sanitisePlayer $ boardPlayer2 b}
+    2 -> b { boardPlayer1 = sanitisePlayer $ boardPlayer1 b}
