68ca082414aa4a1acacaf550e4a33847278c6163
Oscar Harris <ozj.harris@gmail.com>
2020-05-17 17:27:07 +0100

    Add `GameStates` definition

--- src/Purestone/Server/GameState.hs
+++ src/Purestone/Server/GameState.hs
@@ -7,8 +7,12 @@
 module Purestone.Server.GameState where
 
 import Data.Time.Clock
+import Data.IntMap
 
 import Purestone.Board
+
+-- | Represents all the current GameStates in the form of an IntMap
+type GameStates = IntMap GameState
 
 -- | Represents the current board, whether player 1/player 2 has yet to receive the 
 --   most recent changes and which player's turn it is
