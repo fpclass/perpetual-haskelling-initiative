99cc164c249e22f5f1fdacea0c0f30cd1c1203b4
Oscar Harris <ozj.harris@gmail.com>
2020-05-17 18:07:33 +0100

    Remove Maybe from board in `GameState`

--- src/Purestone/Server/GameState.hs
+++ src/Purestone/Server/GameState.hs
@@ -16,4 +16,4 @@ type GameStates = IntMap GameState
 
 -- | Represents the current board, whether player 1/player 2 has yet to receive the 
 --   most recent changes and which player's turn it is
-type GameState = (Maybe Board, UTCTime, Int)
+type GameState = (Board, UTCTime, Int)
