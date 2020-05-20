9815546864b93e8ea50196693a9912e9d2c98610
Oscar Harris <ozj.harris@gmail.com>
2020-05-20 16:34:03 +0100

    Move Interpreter to Purestone.Language

--- src/Purestone/Language/Interpreter.hs
+++ src/Purestone/Language/Interpreter.hs
@@ -5,7 +5,7 @@
 -- LICENSE file in the root directory of this source tree.                   --
 -------------------------------------------------------------------------------
 
-module Purestone.Interpreter where
+module Purestone.Language.Interpreter where
 
 -------------------------------------------------------------------------------
 
